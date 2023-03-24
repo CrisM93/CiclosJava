89f1f72c7ea28f9aa98ec2e29f998e45f3331ac1
CrisM93 <criztian.gio@gmail.com>
2023-03-23 21:44:11 -0600

    Ciclos en Java

---
+++ src/ciclosjava/Ciclosjava.java
@@ -0,0 +1,30 @@
+package ciclosjava;
+
+/**
+ *
+ * @author crizt
+ */
+public class Ciclosjava {
+
+    public static void main(String[] args) {
+        //while
+        /* var contador = 0;
+        while (contador < 3) {
+            System.out.println("Contador = " + contador);
+            contador++;
+        }*/
+        //Al menos una vez se ejecuta el do-while
+//        var contador = 0;
+//        do {
+//            System.out.println("Contador " + contador);
+//            contador++;
+//        } while (contador < 1);
+//ciclo for
+        for (int i = 1; i <= 5; i++) {
+            if (i % 2 == 0) {
+                continue;//va la siguiente iteracion
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
